e1af69ea90866c0e29aa222f0233080278c99106
Nida <nidakul@hotmail.com>
2024-06-07 17:03:39 +0300

    created MsSqlConfiguration

---
+++ src/userActivityReportSystem/Core/CrossCuttingConcerns/Logging/SeriLog/Logger/MsSqlLogger.cs
@@ -0,0 +1,24 @@
+﻿using Core.CrossCuttingConcerns.Logging.SeriLog.ConfigurationModels;
+using NArchitecture.Core.CrossCuttingConcerns.Logging.Serilog;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+namespace Core.CrossCuttingConcerns.Logging.SeriLog.Logger
+{
+    public class MsSqlLogger : SerilogLoggerServiceBase
+    {
+        public MsSqlLogger(MsSqlConfiguration configuration) : base(logger: null!)
+        {
+            Logger = new LoggerConfiguration()
+                .WriteTo.MSSqlServer(
+                    connectionString: "Server=localhost; Database=UserActivityReportSystem; User Id=SA; Password=rentacardb; TrustServerCertificate=True;",
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = configuration.TableName,
+                        AutoCreateSqlTable = configuration.AutoCreateSqlTable
+                    })
+                .CreateLogger();
+        }
+    }
+}
+
